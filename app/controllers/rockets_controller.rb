f77474218a8f234aef1411185249b28133c28e6d
jillmcafee <jill.mcafee@vanderbilt.edu>
2011-03-18 09:36:30 -0500

    fixed header

--- app/controllers/rockets_controller.rb
+++ app/controllers/rockets_controller.rb
@@ -1,9 +1,9 @@
 class RocketsController < ApplicationController
   
   def index
-    @title = "Rocket Math Practce"
-    @upfactors = [0,1,2,3,4,5,6,7,8,9,10,11,12]
-    @downfactors = [0,1,2,3,4,5,6,7,8,9,10,11,12]
+    @title = "Rocket Math Practice"
+    @upfactors = [0,1,2,3,4,5,6,7,8,9,10]
+    @downfactors = [0,1,2,3,4,5,6,7,8,9,10]
     
     @upfactors = @upfactors.sort_by{rand}
     @downfactors = @downfactors.sort_by{rand}
